3a3e717760d74922131acd745a713a17d828ddc6
Vladislav <vladikyssjil@gmail.com>
2019-03-01 01:17:17 +0200

    version 2.0

--- src/app.js
+++ src/app.js
@@ -1,93 +1,89 @@
-document.addEventListener('DOMContentLoaded', startMonth);
 document.addEventListener('DOMContentLoaded', switchMonth);
-let date = new Date();
-let dayOfMonth, dayOfTheWeek, qualityDayInMonth, today = new Date(), day;
-
-function startMonth() {
-    date = new Date(date.getFullYear(), date.getMonth(), 1);
-    document.querySelector('.month').innerHTML = months[date.getMonth()]; //преобразовать номер месяца словом и вывести его
-    document.querySelector('.year').innerHTML = date.getFullYear(); //вывод года
-    dayOfTheWeek = date.getDay();
-
-    let i = 1;
-    while (i < dayOfTheWeek && i < 7 || dayOfTheWeek == 0 && i < 7) { //добавляем пустые div-ы
-        dayOfMonth = document.createElement("DIV");
-        dayOfMonth.className = 'date';
-        document.getElementById("dayInMonth").appendChild(dayOfMonth);
-        i++;
-    }
-
-    qualityDayInMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate(); //узнать сколько дней в текущем месяце
-    for (let i = 1; i <= qualityDayInMonth; i++) {
-        //заполнение месяца
-        dayOfMonth = document.createElement("DIV");
-        dayOfMonth.innerHTML = i;
-        dayOfMonth.className = 'date';
-        document.getElementById("dayInMonth").appendChild(dayOfMonth);
-        day = today.getDate();
-        if (i == day) { //отметить сегоднешний день цветом
-            dayOfMonth.className = 'today';
-        }
-    }
-}
 
 function switchMonth() {   //переключатель месяцов
+    let date = new Date();
+    changeMonth(date); //первый месяц который появится при обновлении страницы
+    date = new Date(date.getFullYear(), date.getMonth(), 1);
     this.addEventListener('click', function (e) {
         let switchMonths = e.target.getAttribute("data-index");
-        if (switchMonths == 'right') { //если месяц назад
-            date = new Date(date.getFullYear(), date.getMonth() - 1, 1);
-        } else if (switchMonths == 'left') { //если месяц вперёд
-            date = new Date(date.getFullYear(), date.getMonth() + 1, 1);
+        if (switchMonths == 'next') { //если месяц вперёд
+            date.setMonth(date.getMonth() + 1);
+
+        } else if (switchMonths == 'prev') { //если месяц назад
+            date.setMonth(date.getMonth() - 1);
         }
+        console.log('after ', date);
         if (e.target.classList.contains('arrow')) { //если нажатие было на стрелку, то вызываем функцию
-            changeMonth();
+            removeMonth();
+            changeMonth(date);
         }
     })
 }
 
-function changeMonth() {
-    let removeMonth = document.querySelectorAll('.date');
-    removeMonth.forEach(
-        function (day) {
-            day.remove()
-        }); //удаляем предыдущий месяц
-    let removeToday = document.querySelectorAll('.today');
-    removeToday.forEach(
-        function (today) {
-            today.remove()
-        }
-    ); //удаляем выделение текущего дня
+function removeMonth() {
+    let monthToRemove = document.querySelector('.flex');
+    monthToRemove.innerHTML = '';
+}
 
+function changeMonth(date) {
+    let dayOfMonth, dayOfTheWeek, qualityDayInMonth, day, today = new Date();
+
+    date = new Date(date.getFullYear(), date.getMonth(), 1);
     document.querySelector('.month').innerHTML = months[date.getMonth()];
     document.querySelector('.year').innerHTML = date.getFullYear();
 
-    dayOfTheWeek = date.getDay();
+    dayOfTheWeek = date.getDay(); //какой день недели 1 число месяца
     let i = 1;
-    while (i < dayOfTheWeek && i < 7 || dayOfTheWeek == 0 && i < 7) {
-        dayOfMonth = document.createElement("DIV");
-        dayOfMonth.className = 'date';
-        document.getElementById("dayInMonth").appendChild(dayOfMonth);
+    do {     // добавляем дни с предыдущего месяца
+        if (dayOfTheWeek == 0) {
+            dayOfTheWeek = 7;
+            date.setDate(date.getDate() - dayOfTheWeek + i);
+        } else if (dayOfTheWeek > 1) {
+            date.setDate(date.getDate() - dayOfTheWeek + i);
+        } else if (dayOfTheWeek == 1) {
+            dayOfTheWeek = 8;
+            date.setDate(date.getDate() - dayOfTheWeek + i);
+        }
+        addAndFillPrevMonth(date,dayOfMonth);
+        date = new Date(date.getFullYear(), date.getMonth() + 1, 1);
         i++;
-    }
+    } while (i < dayOfTheWeek && i <= 7 || dayOfTheWeek == 0 && i <= 6);
 
-    qualityDayInMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+    qualityDayInMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate(); //узнать сколько дней в текущем месяце
     for (let i = 1; i <= qualityDayInMonth; i++) {
-        dayOfMonth = document.createElement("DIV");
-        dayOfMonth.innerHTML = i;
-        dayOfMonth.className = 'date';
-        document.getElementById("dayInMonth").appendChild(dayOfMonth);
+        //заполнение месяца
+       addAndFillCurrentMonth(day,dayOfMonth,i,today);
+
+    }
+    let dayFromNextMonth = 43 - qualityDayInMonth - dayOfTheWeek;
+    for (let i = 1; i <= dayFromNextMonth; i++) {  //добавить дни с следующего месяца в пустые места
+        addAndFillNextMonth(dayOfMonth,i);
     }
 }
 
-let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+function addAndFillPrevMonth(date,dayOfMonth) { // добавляем и заполняем предыдущий месяц
+    dayOfMonth = document.createElement("DIV");
+    dayOfMonth.innerHTML = date.getDate();
+    dayOfMonth.className = 'no-active';
+    document.getElementById("dayInMonth").appendChild(dayOfMonth);
+}
 
+function addAndFillCurrentMonth(day,dayOfMonth,i,today) { // добавляем и заполняем текущий месяц
+    dayOfMonth = document.createElement("DIV");
+    dayOfMonth.innerHTML = i;
+    dayOfMonth.className = 'date';
+    document.getElementById("dayInMonth").appendChild(dayOfMonth);
+    day = today.getDate();
+    if (i == day) { //отметить сегоднешний день цветом
+        dayOfMonth.className = 'today';
+    }
+}
 
-// console.log(x);
-// console.log(x.getDate()); //день
-// console.log(x.getDay());    //день в тижні
-// console.log(x.getMonth());  //місяць
-// console.log(month[x.getMonth()]);  //місяць
-// console.log(x.getFullYear());  //рік
-//сгенерировать 7 div, первой недели с числами.
-// let prororor = new Date(2019,12,0).getDate();
-// console.log("TEST= "+prororor);
+function addAndFillNextMonth(dayOfMonth,i) { //добавляем и заполняем следующий месяц
+    dayOfMonth = document.createElement("DIV");
+    dayOfMonth.innerHTML = i;
+    dayOfMonth.className = 'no-active';
+    document.getElementById("dayInMonth").appendChild(dayOfMonth);
+}
+
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
